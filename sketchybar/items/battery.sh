ee1c4a242f3e3ceb82b6bff68e1d30f19a0492f9
Niko Kivelä <niko@tovrleaf.com>
2025-11-02 21:26:21 +0200

    Add battery script to sketchybar and divider

---
+++ sketchybar/items/battery.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+source "lib/functions.sh"
+
+battery=(
+  update_freq=1
+  script="$PLUGIN_DIR/battery.sh"
+)
+
+battery_details=(
+	background.corner_radius=12
+	background.padding_left=5
+	background.padding_right=10
+	icon.background.height=2
+	icon.background.y_offset=-12
+)
+
+sketchybar -m --add item battery right \
+  --set battery "${battery[@]}" \
+  --subscribe battery "${popup_events[@]}" mouse.clicked \
+  \
+  --add item battery.details popup.battery  \
+  --set battery.details "${battery_details[@]}"   
