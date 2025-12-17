4d0a6c2839e17d340877732c4bbd22b55332ef67
Niko Kivelä <niko@tovrleaf.com>
2025-12-18 00:02:46 +0200

    Move global gitconfig under .config

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-ln -sf Brewfile ~/Brewfile
+ln -sf ~/.config/Brewfile ~/Brewfile
+ln -sf ~/.config/.gitconfig ~/.gitconfig
+ln -sf ~/.config/.gitconfig.work ~/.gitconfig.work
