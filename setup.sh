4f580e1ccc75f9fbd59cf9f00bdf2d66ab7ba4ed
Niko Kivelä <niko@tovrleaf.com>
2025-12-27 12:09:27 +0200

    Refactor tmux config

--- setup.sh
+++ setup.sh
@@ -7,4 +7,3 @@ cd "$(dirname "$0")"
 ln -sf ~/.config/Brewfile ~/Brewfile
 ln -sf ~/.config/.gitconfig ~/.gitconfig
 ln -sf ~/.config/.gitconfig.work ~/.gitconfig.work
-ln -sf ~/.config/tmux/tmux.conf ~/.tmux.conf
